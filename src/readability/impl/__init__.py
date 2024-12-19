3072e17a7af219093ef6d9a2aef4b9d49e418511
Muspi Merol <me@promplate.dev>
2024-12-19 11:12:09 +0800

    chore: rename `non_pyodide` implementation to `pythonmonkey`

--- src/readability/impl/__init__.py
+++ src/readability/impl/__init__.py
@@ -3,6 +3,6 @@ from sys import platform
 if platform == "emscripten":
     from .pyodide import parse
 else:
-    from .non_pyodide import parse
+    from .pythonmonkey import parse
 
 __all__ = ["parse"]
